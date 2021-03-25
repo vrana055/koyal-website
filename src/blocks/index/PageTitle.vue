fb0f7173c8873d01c321a2ae622da238c926530c
sahil2760 <sahilvengurlekar50@gmail.com>
2021-03-25 21:44:37 +0530

    changes made into header section

--- src/blocks/index/PageTitle.vue
+++ src/blocks/index/PageTitle.vue
@@ -1,39 +1,10 @@
 <template>
-    <section id="page-title" class="block with-img with-service-items" style="background-image: url('assets/img/placeholder/1920x1000.jpg')">
+    <section id="page-title" class="block with-img with-service-items" style="background-image: url('assets/img/placeholder/Home-Banner.jpg')">
         <div class="wrapper d-flex flex-column justify-content-center">
             <div class="page-title-body page-title-body-space-left">
                 <div class="title">
-                    <h1 class="large">Business <br>Consulting Agency</h1>
-                </div>
-
-                <div class="description spacer p-top-lg">
-                    <p>We are digital business consulting agency that helps companies increase better incomes and help with branding for faster business growing.</p>
-                </div>
-
-                <div class="spacer p-top-lg">
-                    <router-link to="/about-us" class="btn btn-outline-tertiary">Learn More</router-link>
-                </div>
-            </div>
-
-            <div class="page-title-footer full-width">
-                <div class="d-flex justify-content-between">
-                    <a href="#services" class="scroll-to-id scroll-animation">
-                        <i class="malex-icon-long-arrow-down i-large-2"></i>
-                    </a>
-
-                    <div class="align-self-end">
-                        <nav class="languages">
-                            <ul class="nav">
-                                <li class="lang-item current-lang">
-                                    <router-link title="En" to="/">En</router-link>
-                                </li>
-
-                                <li class="lang-item">
-                                    <router-link title="Ru" to="/">Ru</router-link>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+                    <h1 class="large">RELIABLE OFFSHORE <br>TECHNOLOGY PARTNER  </h1> <br>
+                    <p>Learn More</p>
                 </div>
             </div>
         </div>
